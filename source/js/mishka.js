233698d81f94c36aef76d8607bcd322991858438
AGA <nedrairknah@gmail.com>
2020-05-28 00:24:38 +1000

    3Attempt

--- source/js/mishka.js
+++ source/js/mishka.js
@@ -1,25 +1,8 @@
 "use strict"
 
-// function showMe() {
-//    // alert('Jopa!');
-// }
-
-//  showMe();
-
-// function toggleMenu(evt) {
-//   var menu = document.querySelector('.site-list');
-//   // evt.preventDefault();
-//   if (menu.classList.contains('site-list--closed')) {
-//     menu.classList.remove('site-list--closed');
-//   } else {
-//     menu.classList.add('site-list--closed');
-//   }
-// }
-
+var menu = document.querySelector('.site-list');
 var button = document.querySelector('.page-header__toggle');
 button.addEventListener('click', function toggleMenu() {
-  var menu = document.querySelector('.site-list');
-  // evt.preventDefault();
   if (menu.classList.contains('site-list--closed')) {
     menu.classList.remove('site-list--closed');
     button.classList.add('page-header__toggle--opened');
@@ -28,3 +11,11 @@ button.addEventListener('click', function toggleMenu() {
     button.classList.remove('page-header__toggle--opened');
   }
 });
+
+function closeMenu() {
+  menu.classList.add('site-list--closed');
+  button.classList.remove('page-header__toggle--hide');
+  button.classList.remove('page-header__toggle--opened');
+
+}
+closeMenu()
